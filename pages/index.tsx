1f1a9c0bb2f8cc6f4e653a7a2984164404ff932e
Radek Pleskac <info@radekpleskac.com>
2021-12-01 22:42:13 +0100

    Add Chakra UI, basic Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,18 @@
-// @ts-nocheck
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import { useState } from 'react'
+import { Box } from '@chakra-ui/react'
+
 import Layout from '../components/Layout/Layout'
-import Library from '../components/Library/Library'
-import Canvas from '../components/Canvas/Canvas'
-import type { PlantType } from '../../types'
-import { v4 as uuidv4 } from 'uuid'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const [plants, setPlants] = useState([])
-
-  const addPlants = (newPlant: PlantType, monitor: any) => {
-    const plantId: string = uuidv4()
-    const plant = {
-      ...newPlant,
-      id: plantId,
-    }
-    setPlants((prevPlants) => [...prevPlants, plant])
-  }
-
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>My Orchard</title>
-        <meta name="description" content="application to manage your orchard" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Layout
-        sidebar={<Library />}
-        main={<Canvas plants={plants} dropHandler={addPlants} />}
-      ></Layout>
-    </div>
+    <Layout>
+      <div className={styles.container}>
+        <Box bg="tomato" w="100%" p={4} color="white">
+          <h1>Dashboard - TBD</h1>
+        </Box>
+      </div>
+    </Layout>
   )
 }
 
---
+++ pages/orchard/index.tsx
@@ -0,0 +1,41 @@
+// @ts-nocheck
+import React, { useState, ReactNode } from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { v4 as uuidv4 } from 'uuid'
+
+import type { PlantType } from '../../types'
+import Layout from '../../components/Layout/Layout'
+import Library from '../../components/Library/Library'
+import Canvas from '../../components/Canvas/Canvas'
+import classes from './index.module.css'
+
+function Orchard() {
+  const [plants, setPlants] = useState([])
+
+  const addPlants = (newPlant: PlantType, monitor: any) => {
+    const plantId: string = uuidv4()
+    const plant = {
+      ...newPlant,
+      id: plantId,
+    }
+    setPlants((prevPlants) => [...prevPlants, plant])
+  }
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Layout>
+        <main className={classes.grid}>
+          <DndProvider backend={HTML5Backend}>
+            <main className={classes.grid}>
+              <Library />
+              <Canvas plants={plants} dropHandler={addPlants} />
+            </main>
+          </DndProvider>
+        </main>
+      </Layout>
+    </DndProvider>
+  )
+}
+
+export default Orchard
