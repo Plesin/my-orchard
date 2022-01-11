a95bd19a46a87cdb4343cdb1647f558b918f51ae
Radek Pleskac <info@radekpleskac.com>
2022-01-11 22:32:09 +0100

    jest setup and initial test

---
+++ pages/settings/ColorModeForm.tsx
@@ -0,0 +1,30 @@
+import {
+  Stack,
+  Radio,
+  RadioGroup,
+  useColorMode,
+  FormControl,
+  FormLabel,
+} from '@chakra-ui/react'
+
+function ColorModeForm() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const onChange = (value: string) => {
+    if (colorMode !== value) {
+      toggleColorMode()
+    }
+  }
+  return (
+    <FormControl as="fieldset" m={'2rem 0'}>
+      <FormLabel>Color Mode</FormLabel>
+      <RadioGroup onChange={onChange} value={colorMode}>
+        <Stack direction="row">
+          <Radio value="light">Light</Radio>
+          <Radio value="dark">Dark</Radio>
+        </Stack>
+      </RadioGroup>
+    </FormControl>
+  )
+}
+
+export default ColorModeForm
--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,37 +1,14 @@
 import React from 'react'
-import {
-  Box,
-  Heading,
-  Stack,
-  Radio,
-  RadioGroup,
-  useColorMode,
-  FormControl,
-  FormLabel,
-} from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import Layout from '../../components/Layout/Layout'
+import ColorModeForm from './ColorModeForm'
 
 export default function SettingsPage() {
-  const { colorMode, toggleColorMode } = useColorMode()
-  const onChange = (value: string) => {
-    console.log(colorMode, value)
-    if (colorMode !== value) {
-      toggleColorMode()
-    }
-  }
   return (
     <Layout>
       <Box p={'2rem 0'}>
         <Heading as="h1">Settings Page</Heading>
-        <FormControl as="fieldset" m={'2rem 0'}>
-          <FormLabel as="legend">Color Mode</FormLabel>
-          <RadioGroup onChange={onChange} value={colorMode}>
-            <Stack direction="row">
-              <Radio value="light">Light</Radio>
-              <Radio value="dark">Dark</Radio>
-            </Stack>
-          </RadioGroup>
-        </FormControl>
+        <ColorModeForm />
       </Box>
     </Layout>
   )
