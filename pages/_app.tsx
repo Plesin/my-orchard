90fe56751b5aeb2bdaceaad2dd73b3c1703fdac3
Radek Pleskac <info@radekpleskac.com>
2022-02-21 15:38:49 +0100

    Add Redux, Redux Toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
+import store from '../redux/store'
 import { makeServer } from '../mirage'
 
 if (process.env.NODE_ENV === 'development') {
@@ -8,9 +10,11 @@ if (process.env.NODE_ENV === 'development') {
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   )
 }
 
--- pages/trees/index.tsx
+++ pages/trees/index.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { useAppSelector, useAppDispatch } from '../../redux/hooks'
+import { loadTrees, selectTrees, selectStatus } from '../../redux/treesSlice'
 import {
   Button,
   Table,
@@ -11,6 +13,8 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  Spinner,
+  Stack,
 } from '@chakra-ui/react'
 import { ChevronDownIcon } from '@chakra-ui/icons'
 import Layout from '../../components/Layout/Layout'
@@ -18,55 +22,65 @@ import H2 from '../../components/H2'
 import type { Tree } from '../../types'
 
 function Trees() {
-  const [trees, setTrees] = useState([])
+  const dispatch = useAppDispatch()
+  const trees = useAppSelector(selectTrees)
+  const status = useAppSelector(selectStatus)
+
   useEffect(() => {
-    fetch('/api/trees')
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json)
-        setTrees(json.trees)
-      })
-  }, [])
+    dispatch(loadTrees())
+  }, [dispatch])
 
   return (
     <Layout>
       <main>
         <H2>All Trees</H2>
-        <Table variant="striped" colorScheme="teal">
-          <Thead>
-            <Tr>
-              <Th>Name</Th>
-              <Th>Type</Th>
-              <Th>Subtype</Th>
-              <Th>Date Planted</Th>
-              <Th>Orchard Id</Th>
-              <Th>Actions</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {trees.map((tree: Tree) => (
-              <Tr key={tree.id}>
-                <Td>{tree.name}</Td>
-                <Td>{tree.type}</Td>
-                <Td>{tree.subtype}</Td>
-                <Td>{tree.datePlanted}</Td>
-                <Td>{tree.orchardId}</Td>
-                <Td>
-                  <Menu>
-                    <MenuButton
-                      as={Button}
-                      rightIcon={<ChevronDownIcon />}
-                    ></MenuButton>
-                    <MenuList>
-                      <MenuItem>Edit</MenuItem>
-                      <MenuItem>Delete</MenuItem>
-                    </MenuList>
-                  </Menu>
-                </Td>
+        {status === 'loading' ? (
+          <Stack direction="row" spacing={4} justifyContent="center">
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="teal.700"
+              color="green.600"
+              size="xl"
+            />
+          </Stack>
+        ) : (
+          <Table variant="striped" colorScheme="teal">
+            <Thead>
+              <Tr>
+                <Th>Name</Th>
+                <Th>Type</Th>
+                <Th>Subtype</Th>
+                <Th>Date Planted</Th>
+                <Th>Orchard Id</Th>
+                <Th>Actions</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
+            </Thead>
+            <Tbody>
+              {trees.map((tree: Tree) => (
+                <Tr key={tree.id}>
+                  <Td>{tree.name}</Td>
+                  <Td>{tree.type}</Td>
+                  <Td>{tree.subtype}</Td>
+                  <Td>{tree.datePlanted}</Td>
+                  <Td>{tree.orchardId}</Td>
+                  <Td>
+                    <Menu>
+                      <MenuButton
+                        as={Button}
+                        rightIcon={<ChevronDownIcon />}
+                      ></MenuButton>
+                      <MenuList>
+                        <MenuItem>Edit</MenuItem>
+                        <MenuItem>Delete</MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        )}
       </main>
     </Layout>
   )
