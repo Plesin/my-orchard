f03dbeac407a56d0a3d57c94aac209fca1fd3708
Radek Pleskac <info@radekpleskac.com>
2022-03-14 18:03:06 +0100

    exluding cypress in jest

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
